32b231bc0d9d40d09d987127e6565c781f715653
Siddharth Sharma <siddharthsharmaofficial2@gmail.com>
2026-01-03 23:09:53 +0530

    Jan 03, 26 Sol

---
+++ GeeksForGeeks/Jan_03_2026.cpp
@@ -0,0 +1,42 @@
+// https://www.geeksforgeeks.org/problems/flattening-a-linked-list/1
+
+class Solution
+{
+private
+    Node merge(Node a, Node b)
+    {
+        if (a == null)
+            return b;
+        if (b == null)
+            return a;
+
+        Node result;
+
+        if (a.data < b.data)
+        {
+            result = a;
+            result.bottom = merge(a.bottom, b);
+        }
+        else
+        {
+            result = b;
+            result.bottom = merge(a, b.bottom);
+        }
+
+        result.next = null;
+        return result;
+    }
+
+public
+    Node flatten(Node root)
+    {
+        if (root == null || root.next == null)
+            return root;
+
+        root.next = flatten(root.next);
+
+        root = merge(root, root.next);
+
+        return root;
+    }
+}
